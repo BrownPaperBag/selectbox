71eea6e9b5959748bfc303086267e86935a94e97
Oran Fry <oran@brownpaperbag.co.nz>
2014-03-31 15:26:43 +1300

    proper js

--- directives/selectbox.js
+++ directives/selectbox.js
@@ -7,15 +7,13 @@ angular.module('selectbox', [])
                 model: '='
             },
             template:
-                '<div class="selectbox">
-                    <div style="position:fixed; top: 0; bottom: 0; left: 0; right: 0" class="selectbox-overlay" ng-show="selecting" ng-click="selecting=0"></div>
-                    <div class="selectbox-value" ng-click="selecting=!selecting">{{options[model]}}</div>
-                    <div class="selectbox-options" ng-show="selecting">
-                        <div
-                            class="selectbox-option"
-                            ng-repeat="(code, description) in options"
-                            ng-click="$parent.model=code; $parent.selecting=0">{{description}}</div>
-                    </div>
-                </div>'
+                '<div class="selectbox">' +
+                '    <div style="position:fixed; top: 0; bottom: 0; left: 0; right: 0" class="selectbox-overlay" ng-show="selecting" ng-click="selecting=0"></div>' +
+                '    <div class="selectbox-value" ng-click="selecting=!selecting">{{options[model]}}</div>' +
+                '    <div class="selectbox-options" ng-show="selecting">' +
+                '        <div class="selectbox-option" ng-repeat="(code, description) in options"' +
+                '            ng-click="$parent.model=code; $parent.selecting=0">{{description}}</div>' +
+                '    </div>' +
+                '</div>'
         }
     });
