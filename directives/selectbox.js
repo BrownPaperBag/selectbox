0ceb2879240386d1d0516dd33564dbe3ee41abb9
Oran Fry <oran@brownpaperbag.co.nz>
2014-06-12 11:03:47 +1200

    development of the module

--- directives/selectbox.js
+++ directives/selectbox.js
@@ -1,19 +1,42 @@
 angular.module('selectbox', [])
     .directive('selectbox', function () {
         return {
+            replace : true,
             restrict: 'E',
             scope: {
                 options: '=',
-                ngModel: '=model',
+                model: '='
             },
             template:
                 '<div class="selectbox">' +
                 '    <div style="position:fixed; top: 0; bottom: 0; left: 0; right: 0" class="selectbox-overlay" ng-show="selecting" ng-click="selecting=0"></div>' +
-                '    <div class="selectbox-value" ng-click="selecting=!selecting">{{options[model]}}</div>' +
+                '    <div class="selectbox-value ng-class: {placeholder: !lookup[model]}" ng-click="selecting=!selecting">{{lookup[model] || placeholder}}</div>' +
                 '    <div class="selectbox-options" ng-show="selecting">' +
-                '        <div class="selectbox-option" ng-repeat="(code, description) in options"' +
-                '            ng-click="$parent.model=code; $parent.selecting=0; valueChanged()">{{description}}</div>' +
+                '        <div class="selectbox-option" ng-repeat="option in options"' +
+                '            ng-click="$parent.model=option.code; $parent.selecting=0">{{option.description}}</div>' +
                 '    </div>' +
-                '</div>'
+                '</div>',
+
+            link : function($scope, $element, $attributes){
+
+                $scope.placeholder = $attributes.placeholder;
+
+                $scope.$watch('model', function(newValue, oldValue){
+
+                    if(newValue != oldValue){
+
+                        $scope.$parent.$eval($attributes.onChange);
+
+                    }
+
+                });
+
+                $scope.$watchCollection('options', function(newValue, oldValue){
+                    $scope.lookup = createLookup($scope.options, 'code', 'description');
+                });
+
+            }
+
         }
+
     });
