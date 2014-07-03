68ecb2042e241112806a38b7a00a2d01dc46c8ff
Eduardo Agostini <edu.agostini@gmail.com>
2014-07-03 12:12:07 +1200

    Width
    New argument called “width”.

--- directives/selectbox.js
+++ directives/selectbox.js
@@ -5,13 +5,14 @@ angular.module('selectbox', [])
             restrict: 'E',
             scope: {
                 options: '=',
-                model: '='
+                model: '=',
+                width: '='
             },
             template:
                 '<div class="selectbox">' +
                 '    <div style="position:fixed; top: 0; bottom: 0; left: 0; right: 0" class="selectbox-overlay" ng-show="selecting" ng-click="selecting=0"></div>' +
-                '    <div class="selectbox-value ng-class: {placeholder: !lookup[model]}" ng-click="selecting=!selecting">{{lookup[model] || placeholder}}</div>' +
-                '    <div class="selectbox-options" ng-show="selecting">' +
+                '    <div class="selectbox-value ng-class: {placeholder: !lookup[model]}" ng-click="selecting=!selecting" ng-style="style.value">{{lookup[model] || placeholder}}</div>' +
+                '    <div class="selectbox-options" ng-show="selecting" ng-style="style.options">' +
                 '        <div class="selectbox-option" ng-repeat="option in options"' +
                 '            ng-click="$parent.model=option.code; $parent.selecting=0">{{option.description}}</div>' +
                 '    </div>' +
@@ -20,6 +21,14 @@ angular.module('selectbox', [])
             link : function($scope, $element, $attributes){
 
                 $scope.placeholder = $attributes.placeholder;
+
+                $scope.style = {
+
+                    options : {},
+
+                    value : {}
+
+                };
 
                 $scope.$watch('model', function(newValue, oldValue){
 
@@ -34,6 +43,13 @@ angular.module('selectbox', [])
                 $scope.$watchCollection('options', function(newValue, oldValue){
                     $scope.lookup = createLookup($scope.options, 'code', 'description');
                 });
+
+                if($scope.width){
+
+                    $scope.style.options.width = $scope.width + 'px';
+                    $scope.style.value.width = $scope.width + 'px';
+
+                }
 
             }
 
