6bb2eca46b66a9c92671a3d56513c7382d931b86
Oran Fry <oran@brownpaperbag.co.nz>
2014-07-30 16:46:16 +1200

    added variation of the selectbox

--- directives/selectbox.js
+++ directives/selectbox.js
@@ -55,4 +55,63 @@ angular.module('selectbox', [])
 
         }
 
+    })
+
+    .directive('valueSelectbox', function () {
+        return {
+            replace : true,
+            restrict: 'E',
+            scope: {
+                options: '='
+                width: '='
+            },
+            template:
+                '<div class="selectbox" ng-style="style.main">' +
+                '    <div style="position:fixed; top: 0; bottom: 0; left: 0; right: 0" class="selectbox-overlay" ng-show="selecting" ng-click="selecting=0"></div>' +
+                '    <div class="selectbox-value ng-class: {placeholder: !lookup[val]}" ng-click="selecting=!selecting">{{lookup[val] || placeholder}}</div>' +
+                '    <div class="selectbox-options" ng-show="selecting" ng-style="style.options">' +
+                '        <div class="selectbox-option" ng-repeat="option in options"' +
+                '            ng-click="$parent.selecting=0; changed(option.code, $event)">{{option.description}}</div>' +
+                '    </div>' +
+                '</div>',
+
+            link : function($scope, $element, $attributes){
+
+                $scope.placeholder = $attributes.placeholder;
+
+                $scope.style = {
+
+                    main : {},
+
+                    options : {}
+
+                };
+
+                $scope.changed = function($value, $event) {
+                    var $subScope = $scope.$parent.$new();
+                    $subScope.$value = $value;
+                    $subScope.$event = $event;
+                    $subScope.$eval($attributes.onChange);
+                };
+
+                $scope.$parent.$watch($attributes.value, function(newValue, oldValue){
+                    $scope.val = $scope.$parent.$eval($attributes.value);
+                });
+
+
+                $scope.$watchCollection('options', function(newValue, oldValue){
+                    $scope.lookup = createLookup($scope.options, 'code', 'description');
+                });
+
+                if($scope.width){
+
+                    $scope.style.main.width = $scope.width + 'px';
+                    $scope.style.options.width = $scope.width + 'px';
+
+                }
+
+            }
+
+        }
+
     });
